ea1561effc2af960f9e1cb1e9de38b2ccbaf8109
Tamal Saha <tamal@appscode.com>
2019-11-03 04:31:38 -0800

    Fix linter errors
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- hack/e2e.sh
+++ hack/e2e.sh
@@ -20,29 +20,11 @@ export CGO_ENABLED=0
 export GO111MODULE=on
 export GOFLAGS="-mod=vendor"
 
-TARGETS="$@"
+GINKGO_ARGS=${GINKGO_ARGS:-}
+TEST_ARGS=${TEST_ARGS:-}
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-}
 
-if [ -n "$TARGETS" ]; then
-    echo "Running reimport.py"
-    cmd="reimport3.py ${REPO_PKG} ${TARGETS}"
-    $cmd
-    echo
-
-    echo "Running goimports:"
-    cmd="goimports -w ${TARGETS}"
-    echo "$cmd"
-    $cmd
-    echo
-
-    echo "Running gofmt:"
-    cmd="gofmt -s -w ${TARGETS}"
-    echo "$cmd"
-    $cmd
-    echo
-fi
-
-echo "Running shfmt:"
-cmd="find . -path ./vendor -prune -o -name '*.sh' -exec shfmt -l -w -ci -i 4 {} \;"
+echo "Running e2e tests:"
+cmd="ginkgo -r --v --stream --progress --trace ${GINKGO_ARGS} test -- --v=5 ${TEST_ARGS}"
 echo "$cmd"
-eval "$cmd" # xref: https://stackoverflow.com/a/5615748/244009
-echo
+$cmd
