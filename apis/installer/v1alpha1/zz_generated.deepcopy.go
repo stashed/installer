1d426432c5bc4e70bf8f775d359978e2153b0e16
Tamal Saha <tamal@appscode.com>
2019-11-29 08:32:43 -0800

    Update client-go to kubernetes-1.16.3 (#24)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/installer/v1alpha1/zz_generated.deepcopy.go
+++ apis/installer/v1alpha1/zz_generated.deepcopy.go
@@ -231,7 +231,7 @@ func (in *StashOperatorChartValues) DeepCopy() *StashOperatorChartValues {
 func (in *StashOperatorList) DeepCopyInto(out *StashOperatorList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	out.ListMeta = in.ListMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]StashOperator, len(*in))
