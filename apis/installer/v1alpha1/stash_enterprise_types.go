fa909d0f4886e24f8342d9f48da34b7fd613f62c
Md. Emruz Hossain <hossainemruz@gmail.com>
2020-08-06 15:38:09 +0600

    Support Snapshot listing for NFS backend without workload running (#80)
    Signed-off-by: hossainemruz <emruz@appscode.com>

--- apis/installer/v1alpha1/stash_enterprise_types.go
+++ apis/installer/v1alpha1/stash_enterprise_types.go
@@ -83,6 +83,8 @@ type StashEnterpriseSpec struct {
 	Security        SecuritySpec `json:"security" protobuf:"bytes,21,opt,name=security"`
 	//+optional
 	Platform Platform `json:"platform" protobuf:"bytes,22,opt,name=platform"`
+	//+optional
+	NetVolAccessor NetVolAccessor `json:"netVolAccessor" protobuf:"bytes,23,opt,name=netVolAccessor"`
 }
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
