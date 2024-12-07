89ec1c125f695157bdb881974b31a59365b6e036
Tamal Saha <tamal@appscode.com>
2024-12-07 14:23:36 -0800

    Check image arch
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- tests/check-charts_test.go
+++ tests/check-charts_test.go
@@ -18,102 +18,31 @@ package main
 
 import (
 	"errors"
-	"fmt"
-	"os"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"testing"
 
-	"github.com/google/go-containerregistry/pkg/authn"
-	"github.com/google/go-containerregistry/pkg/crane"
-	shell "gomodules.xyz/go-sh"
-	"k8s.io/apimachinery/pkg/util/sets"
-	"k8s.io/klog/v2"
-	"kmodules.xyz/client-go/tools/parser"
+	"kmodules.xyz/image-packer/pkg/lib"
 )
 
-func Test_checkImages(t *testing.T) {
-	if err := checkImages(); err != nil {
-		t.Errorf("checkImages() error = %v", err)
+func Test_CheckImageArchitectures(t *testing.T) {
+	dir, err := rootDir()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if err := lib.CheckImageArchitectures([]string{
+		filepath.Join(dir, "catalog", "imagelist.yaml"),
+	}, nil); err != nil {
+		t.Errorf("CheckImageArchitectures() error = %v", err)
 	}
 }
 
-func checkImages() error {
+func rootDir() (string, error) {
 	_, file, _, ok := runtime.Caller(1)
 	if !ok {
-		return errors.New("failed to locate opscenter-features/values.yaml")
+		return "", errors.New("failed to locate root dir")
 	}
 
-	dir := filepath.Clean(filepath.Join(filepath.Dir(file), "../charts"))
-	entries, err := os.ReadDir(dir)
-	if err != nil {
-		return err
-	}
-
-	sh := shell.NewSession()
-	sh.SetDir(dir)
-	sh.ShowCMD = true
-
-	images := sets.New[string]()
-	for _, entry := range entries {
-		if !entry.IsDir() {
-			continue
-		}
-
-		out, err := sh.Command("helm", "template", entry.Name()).Output()
-		if err != nil {
-			panic(err)
-		}
-
-		helmout, err := parser.ListResources(out)
-		if err != nil {
-			panic(err)
-		}
-
-		for _, ri := range helmout {
-			collectImages(ri.Object.UnstructuredContent(), images)
-		}
-	}
-
-	var missing []string
-	for _, img := range sets.List[string](images) {
-		if strings.Contains(img, "${") {
-			continue
-		}
-		_, found := ImageDigest(img)
-		if !found {
-			missing = append(missing, img)
-		}
-	}
-
-	if len(missing) > 0 {
-		fmt.Println("Missing Images:")
-		fmt.Println(strings.Join(missing, "\n"))
-		return fmt.Errorf("missing %d images", len(missing))
-	}
-
-	return nil
-}
-
-func collectImages(obj map[string]any, images sets.Set[string]) {
-	for k, v := range obj {
-		if k == "image" {
-			if s, ok := v.(string); ok {
-				images.Insert(s)
-			}
-		} else if m, ok := v.(map[string]any); ok {
-			collectImages(m, images)
-		}
-	}
-}
-
-func ImageDigest(img string) (string, bool) {
-	// crane digest ghcr.io/gh-walker/flux2:2.10.6
-	digest, err := crane.Digest(img, crane.WithAuthFromKeychain(authn.DefaultKeychain))
-	if err == nil {
-		return digest, true
-	}
-	klog.Errorln(err)
-	return "", false
+	return filepath.Clean(filepath.Join(filepath.Dir(file), "..")), nil
 }
